eb1b98eb8c226030334afcc513461eb90de8176c
eldiiar_b <eldiiar_b@auca.kg>
2023-02-07 15:34:02 +0600

    some changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> Erbol Kot.
+                    We are starting
                 </p>
                 <a
                     className="App-link"
