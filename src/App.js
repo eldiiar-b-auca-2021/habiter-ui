c8e73e05d9f93922f908a24a6c5b5834e8cad8a8
eldiiar_b <eldiiar_b@auca.kg>
2023-02-09 15:56:30 +0600

    some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
 
 function App() {
+    const name = 'Brad';
+    const x = true;
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    We are starting
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className='container'>
+            <h1>Hello Erbol jetim sen jetim</h1>
         </div>
     );
 }
