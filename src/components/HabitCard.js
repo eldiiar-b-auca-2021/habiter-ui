77da9d3fab365066765f842da4c14151192087c8
eldiiar_b <eldiiar_b@auca.kg>
2023-03-17 15:48:43 +0600

    the habit add card is in process

--- src/components/HabitCard.js
+++ src/components/HabitCard.js
@@ -1,5 +1,6 @@
-import { Card, Form, Input, Button } from 'antd';
+import { Card, Form, Input, Button, Select } from 'antd';
 import { useState } from 'react';
+const { Option } = Select;
 
 const HabitCard = () => {
   const [name, setName] = useState('');
@@ -8,10 +9,17 @@ const HabitCard = () => {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [description, setDescription] = useState('');
-
+  const [repeat, setRepeat] = useState('');
   const handleFormSubmit = () => {
     // handle form submission here
   }
+
+  const repeatOptions = [
+    { value: 'Every day', label: 'Every Day' },
+    { value: 'Every n days', label: 'Every N Days' },
+    { value: 'Once in a week', label: 'Once In A Week' },
+    { value: 'Weekly', label: 'Weekly' },
+  ];
 
   return (
     <Card title="Create Habit" bordered={false} style={{ width: 500 }}>
@@ -30,6 +38,27 @@ const HabitCard = () => {
         </Form.Item>
         <Form.Item label="End Date" name="endDate">
           <Input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item label="Repeat" name="repeat">
+            <Select value={repeat} onChange={(value) => setRepeat(value)} style={{ display: 'flex' }}>
+              {repeatOptions.map((option) => (
+                <Option key={option.value} value={option.value}>
+                  <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+                    <div
+                      style={{
+                        backgroundColor: '#1890ff',
+                        width: '20px',
+                        height: '20px',
+                        borderRadius: '50%',
+                      }}
+                    />
+                    <div>{option.label}</div>
+                  </div>
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
         </Form.Item>
         <Form.Item label="Description" name="description">
           <Input.TextArea value={description} onChange={(e) => setDescription(e.target.value)} />
